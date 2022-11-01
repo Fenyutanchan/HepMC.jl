d47867c63204f7ca9c7f642b069ca851935b26c0
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2022-11-01 15:34:27 +0800

    init for HepMC.jl

--- src/HepMC.jl
+++ src/HepMC.jl
@@ -1,9 +1,11 @@
 module  HepMC
 
-using   StructParticle
+    using   StructParticle
 
-include("Read.jl")
-include("ReadHepMC2.jl")
-include("ReadHepMC3.jl")
+    export  read_HepMC_file
+
+    include("Read.jl")
+    include("ReadHepMC2.jl")
+    include("ReadHepMC3.jl")
 
 end # module HepMC
