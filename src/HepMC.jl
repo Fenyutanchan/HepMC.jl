1df270213ab070310d3311dd20fd667e7220525b
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2022-11-10 15:23:10 +0800

    remove the part of JLD2

--- src/HepMC.jl
+++ src/HepMC.jl
@@ -3,13 +3,8 @@ module  HepMC
     using   JLD2
     using   StructParticle
 
-    export  read_HepMC_file
-    export  read_events_JLD2, write_events_JLD2
-
     include("Read.jl")
     include("ReadHepMC2.jl")
     include("ReadHepMC3.jl")
-
-    include("ForJLD2.jl")
 
 end # module HepMC
