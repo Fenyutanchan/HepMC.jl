0480959a775c1ca43f7a39ca3102f99a887efc73
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2022-11-10 10:41:42 +0800

    add support for JLD2

---
+++ src/ForJLD2.jl
@@ -0,0 +1,28 @@
+function read_events_JLD2(file_name::String, index::String)::Event
+    @assert splitext(file_name)[end] == ".jld2"
+
+    jld_file    =   jldopen(file_name, "r")
+    jld_keys    =   keys(jld_file)
+
+    @assert index ∈ jld_keys
+    event   =   Event(jld_file[index])
+
+    close(jld_file)
+    return  event
+end
+read_events_JLD2(file_name::String, index::Int)::Event  =   read_events_JLD2(
+    file_name, "$index"
+)
+
+function write_events_JLD2(file_name::String, event_list::Vector{Event})::Nothing
+    if splitext(file_name)[end] != ".jld2"
+        file_name   *=  ".jld2"
+    end
+
+    file_content    =   Dict{String, Dict}()
+    for ii ∈ eachindex(event_list)
+        file_content["$ii"]    =   Dict(event_list[ii])
+    end
+    save(file_name, file_content)
+    return  nothing
+end
--- src/HepMC.jl
+++ src/HepMC.jl
@@ -1,11 +1,15 @@
 module  HepMC
 
+    using   JLD2
     using   StructParticle
 
     export  read_HepMC_file
+    export  read_events_JLD2, write_events_JLD2
 
     include("Read.jl")
     include("ReadHepMC2.jl")
     include("ReadHepMC3.jl")
+
+    include("ForJLD2.jl")
 
 end # module HepMC
